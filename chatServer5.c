e1cf4596affa58b94ecad62939aa4f45294f3474
Teegan Swanson <tcswanny@gmail.com>
2022-11-28 22:18:45 -0600

    Attempting to fix issues with connection to directory server

--- chatServer5.c
+++ chatServer5.c
@@ -350,7 +350,7 @@ int startServer()
     memset((char *)&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(SERV_TCP_PORT);
+    serv_addr.sin_port = htons(port);
 
     if (bind(hostSockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
